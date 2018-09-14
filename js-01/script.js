125287cc9dbbf65b943dc45a8415343decc3196c
vainawebuser <vainawebcpx@gmail.com>
2018-09-14 15:47:02 -0300

    javascript

--- js-01/script.js
+++ js-01/script.js
@@ -1,16 +1,55 @@
-document.write("<p>Hello"+"Word<br>Java Script <br></p>");
-document.write(30+50);
+
+// ==================================//
+
+// document.write("<p>Hello"+"Word<br>Java Script <br></p>");
+// document.write(30+50);
 
 
-var nome = "<p><br><span>name:</span> Paulo henrique</p>";
-var nun = "<p> <br><span>ano:</span>2018</p>";
-cep = "<p><br><span>cep:</span>21061240</p>";
-idade = "<p><br><span>idade:</span>21</p>";
+// var nome = "<p><br><span>name:</span> Paulo henrique</p>";
+// var nun = "<p> <br><span>ano:</span>2018</p>";
+// cep = "<p><br><span>cep:</span>21061240</p>";
+// idade = "<p><br><span>idade:</span>21</p>";
 
-var texto=nome+nun+idade+cep;
-document.write(texto);
+// var texto=nome+nun+idade+cep;
+// document.write(texto);
 
-document.write(nome,nun,idade,cep);
-document.write(nun);
-document.write(idade);
-document.write(cep);
+// document.write(nome,nun,idade,cep);
+// document.write(nun);
+// document.write(idade);
+// document.write(cep);
+
+
+// ==================================//
+	//var cor=new Array();
+	// var cor=[];
+
+
+	// cor.push("Preto");
+	// cor.push("Vermelho");
+	// cor.push("Verde");
+	// cor.push("Branco");
+	// cor.push("Roxo");
+	// cor.push("Azul");
+
+
+	// cor[0]="Vermelho";
+	// cor[1]="Azul";
+	// cor[2]="Preto";
+	// cor[3]="Verde";
+	// cor[4]="Roxo";			
+
+	// document.write(cor[2]);
+
+// ==================================//
+
+
+	var itens=["Faca","Pedra","Corda","Lanterna","Luvas","Chave de fenda","Arame"];
+	var mochila=[];
+
+	mochila.push(5);
+	mochila.push(2);
+	mochila.push(3);
+
+	document.write(itens[mochila[0]]+"<br>");
+	document.write(itens[mochila[1]]+"<br>");
+	document.write(itens[mochila[2]]+"<br>");
