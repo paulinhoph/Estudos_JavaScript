63853824e66282cd8edbba6107e8fcac2fcd1d86
paulinhoph <paulinhoph.ph67@gmail.com>
2018-11-23 11:00:35 -0200

    estudos

--- js-01/script.js
+++ js-01/script.js
@@ -1,5 +1,5 @@
 
-// ==================================//
+// ====== Document.Write("Introdução") ========//
 
 // document.write("<p>Hello"+"Word<br>Java Script <br></p>");
 // document.write(30+50);
@@ -19,7 +19,8 @@
 // document.write(cep);
 
 
-// ==================================//
+// ======  Variáveis  ===========//
+
 	//var cor=new Array();
 	// var cor=[];
 
@@ -40,16 +41,225 @@
 
 	// document.write(cor[2]);
 
-// ==================================//
+// ====== Costantes  ========//
 
 
-	var itens=["Faca","Pedra","Corda","Lanterna","Luvas","Chave de fenda","Arame"];
-	var mochila=[];
+	// var itens=["Faca","Pedra","Corda","Lanterna","Luvas","Chave de fenda","Arame"];
+	// var mochila=[];
 
-	mochila.push(5);
-	mochila.push(2);
-	mochila.push(3);
+	// mochila.push("Faca");
+	// mochila.push("Corda");
+	// mochila.push("Pedreira");
+	// mochila.push("Chave");
+	// mochila.push("Lanterna");
+	// mochila.unshift("Pedra");
 
-	document.write(itens[mochila[0]]+"<br>");
-	document.write(itens[mochila[1]]+"<br>");
-	document.write(itens[mochila[2]]+"<br>");
+
+	// mochila.pop();
+
+
+	// mochila.shift();
+	// mochila.splice(2);
+
+	// document.write(mochila[0]+"<br>");
+	// document.write(mochila[1]+"<br>");
+	// document.write(mochila[2]+"<br>");
+	// document.write(mochila[3]+"<br>");
+	// document.write(mochila[4]+"<br>");
+	// document.write(mochila[5]+"<br>");
+
+// ========  Arry/ Vetor - Métodos de Calsse =========//
+
+	// var  mochila=[],mochila1=[],mochila2=[],pos;
+
+
+	// mochila1.push("Faca");
+	// mochila1.push("Corda");
+	// mochila1.push("Pedreira");
+	// mochila1.push("Chave");
+	// mochila1.push("Lanterna");
+	// mochila1.push("Pedra");
+	// mochila1.push("Mouse");
+
+
+	// mochila2.push("Faca");
+	// mochila2.push("Corda");
+	// mochila2.push("Pedreira");
+	// mochila2.push("Chave");
+	// mochila2.push("Lanterna");
+	// mochila2.push("Pedra");
+    
+ //    mochila1.pop();
+
+	// mochila=mochila1.concat(mochila2);
+
+	// document.write(mochila[0]+"<br>");
+	// document.write(mochila[1]+"<br>");
+	// document.write(mochila[2]+"<br>");
+	// document.write(mochila[3]+"<br>");
+	// document.write(mochila[4]+"<br>");
+	// document.write(mochila[5]+"<br>");
+	// document.write(mochila[6]+"<br>");
+	// document.write(mochila[7]+"<br>");
+	// document.write(mochila[8]+"<br>");
+	// document.write(mochila[9]+"<br>");
+	// document.write(mochila[10]+"<br>");
+	// document.write(mochila[11]+"<br>");
+
+	// document.write("Tamanho da mochila 1:" + mochila1.length +"<br>");
+	// document.write("Tamanho da mochila 2:" + mochila2.length +"<br>");
+	// document.write("Tamanho da mochila:" + mochila.length +"<br>");
+
+// =======  IF - ELSE ALERT =============//
+
+	// var num=[5,8,4,9,7,6,3,0,2,1];
+	// var numeros;
+
+	// num.sort();
+	// num.reverse();
+
+	// numeros=num.join();
+
+	// document.write(numeros+"<br>");
+
+	// document.write(num[0]+"<br>");
+	// document.write(num[1]+"<br>");
+	// document.write(num[2]+"<br>");
+	// document.write(num[3]+"<br>");
+	// document.write(num[4]+"<br>");
+	// document.write(num[5]+"<br>");
+	// document.write(num[6]+"<br>");
+	// document.write(num[7]+"<br>");
+	// document.write(num[8]+"<br>");
+	// document.write(num[9]+"<br>");
+
+ // 	mochila.push("Faca");
+	// mochila.push("Corda");
+	// mochila.push("Pedreira");
+	// mochila.push("Chave");
+	// mochila.push("Lanterna");
+	// mochila.push("Pedra");
+
+
+	// pos=mochila.indexOf("mareta");
+
+	// if(pos < 0){
+	// 	alert("item não está na mochula");
+	// }else{
+	// 	alert("item está na mochila",+pos);	
+	// }
+
+// =======  Trabalhando com Matriz  ========//
+
+
+	// var mochila1=new Array();
+
+
+	// var item1=["Pedra",3],
+	// 	item2=["Faca",19],
+	// 	item3=["Lanterna",10],
+	// 	item4=["Luvas",8];
+
+	// mochila1.push(item1);
+	// mochila1.push(item2);
+	// mochila1.push(item3);
+	// mochila1.push(item4);
+
+
+	// document.write(mochila1[0][0] + " - " + mochila1[0][1] + "<br>");
+	// document.write(mochila1[1][0] + " - " + mochila1[1][1] + "<br>");
+	// document.write(mochila1[2][0] + " - " + mochila1[2][1] + "<br>");
+	// document.write(mochila1[3][0] + " - " + mochila1[3][1] + "<br>");
+
+// =======  Jogo   ========//	
+
+// segundos = 7;
+// function contagem_tempo(){	             
+	   
+// document.contador.segundos.value = segundos; 
+// segundos = segundos - 1;
+
+// if (segundos == -1) {
+// segundos = 0;
+// }   
+               
+// timerID = setTimeout("contagem_tempo()",1000);   
+// }   
+
+
+// =======  Contagem  ========//
+
+'use strict'
+			var min = 60,
+				seg = 1;
+
+			function start() {
+
+				if((min == 60) && (seg == 1)){
+					relogio()
+				}
+			}
+
+			function relogio(){		
+
+				if((min > 0) || (seg > 0)){				
+					if(seg == 0){					
+						seg = 59;					
+						min = min - 1	
+					}				
+					else{					
+						seg = seg - 1;				
+					}				
+					if(min.toString().length == 1){					
+						min = "0" + min;				
+					}				
+					if(seg.toString().length == 1){					
+						seg = "0" + seg;				
+					}				
+					document.getElementById('spanRelogio').innerHTML = min + ":" + seg;				
+					setTimeout('relogio()', 1000);		
+					setCookie('tempo',min + ":" + seg,30)
+				}			
+				else{				
+					document.getElementById('spanRelogio').innerHTML = "00:00";		
+					min = 1;
+					seg = 1;	
+				}		
+			}	
+
+			function setCookie(cname,cvalue,exdays) {
+			    var d = new Date();
+			    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+			    var expires = "expires=" + d.toGMTString();
+			    document.cookie = cname+"="+cvalue+"; "+expires;
+			}
+
+			function getCookie(cname) {
+			    var name = cname + "=";
+			    var ca = document.cookie.split(';');
+			    for(var i=0; i<ca.length; i++) {
+			        var c = ca[i];
+			        while (c.charAt(0)==' ') {
+			            c = c.substring(1);
+			        }
+			        if (c.indexOf(name) == 0) {
+			            return c.substring(name.length, c.length);
+			        }
+			    }
+			    return "";
+			}
+
+			function checkCookie() {
+
+			    var cookie = getCookie("tempo");
+
+			    if ( cookie != "" ) {
+
+					var tempo = getCookie('tempo').split(':');
+					min = tempo[0];
+					seg = tempo[1];
+
+					relogio();
+			    } 
+
+			}
--- js-02/script.js
+++ js-02/script.js
@@ -1,10 +1,10 @@
-// ==================================//
+// ========  Cnfirm   =========//
 // var valor=10.3;
 // var mt=Math.ceil(valor);
 // document.write(mt)
 // const paulo="paulo henrique";
 
-// ==================================//
+// =======   alert    =========//
 // document.write(paulo);
 // var  curso="programaçao web";
 // var js="javascript";
@@ -13,14 +13,14 @@
 // document.write(nome);
 // alert(nome);
 
-// ==================================//
+// ======   Prompt  ===========//
 // var  nome=prompt("Digite seu","nome aki");
 // var res=confirm(nome+"Deseja continuar?");
 // alert(res);
 
 // document.write(res);
 
-// ==================================//
+// ====== IF ELSE Alert ==========//
 // var v1,v2,tx1,tx2;]
 // v1=10;
 // v2=10;
@@ -29,13 +29,12 @@
 // if(v1 == v2){
 //   // document.write(tx1);
 //   alert(tx1)
-// }
-// else{
+// }else{
 //   // document.write(tx2);
 //   alert(tx2)
 // }
 
-// ==================================//
+// =======  Operadores Lógicos AND (&&) e OR (||)    ========//
 // var pontos=72;
 // var energia=30;
 
@@ -45,7 +44,7 @@
 // 	document.write("Você Perdeu")
 // }
 
-// ==================================//
+// ========  IF com múltiplos retornos   =========//
 // var nota=100;
 // var res;
 
@@ -72,7 +71,7 @@
 // }	
 // document.write(res);
 
-// ==================================//
+// ====   IF aninhado    ====//
 
 // var num=20;
 
@@ -91,44 +90,161 @@
 
 		// document.write(res);
 
-// ==================================//
-	var pos,msg,pontos;
-	pontos= 1000;
+// ======   Switch case  ======//
+	// var pos,msg,pontos;
+	// pontos= 800;
 
-	if (pontos >= 1000){
-		pos=1;
-	}else if (pontos >= 800) {
-		pos=2;
-	}else if (pontos >= 600) {
-		pos=3;
-	}
-	else{
-		pos=0;
-	}
+	// if (pontos >= 1000){
+	// 	pos=1;
+	// }else if (pontos >= 800) {
+	// 	pos=2;
+	// }else if (pontos >= 600) {
+	// 	pos=3;
+	// }
+	// else{
+	// 	pos=0;
+	// }
 
 
-	switch(pos){
-		case 1:
-				msg="primeiro lugar<br>medalha de ouro";
-				break;
-		case 2:
-				msg="segundo lugar<br>medalha de prata";
-				break;
-		case 3:
-				msg="terceiro lugar<br>medalha de bronze";
-				break;
-		default:
-				msg="nao foi ao podio <br> nao obiteve medalha";
-	}
-	switch(pos){
-		case 1:
-		case 2:
-		case 3:
-			msg+="<br>subiu ao podio";
-			break;
-		default:
-			msg+="<br>nao obiteve colocação";
+	// switch(pos){
+	// 	case 1:
+	// 			msg="primeiro lugar<br>medalha de ouro";
+	// 			break;
+	// 	case 2:
+	// 			msg="segundo lugar<br>medalha de prata";
+	// 			break;
+	// 	case 3:
+	// 			msg="terceiro lugar<br>medalha de bronze";
+	// 			break;
+	// 	default:
+	// 			msg="nao foi ao podio <br> nao obiteve medalha";
+	// }
+	// switch(pos){
+	// 	case 1:
+	// 	case 2:
+	// 	case 3:
+	// 		msg+="<br>subiu ao podio";
+	// 		break;
+	// 	default:
+	// 		msg+="<br>nao obiteve colocação";
 		
-	}
+	// }
 
-	document.write(msg);
+	// document.write(msg);
+
+
+
+
+
+
+// ======           DOM               ======//
+
+
+
+
+
+// ======  DOM GetElemetsById  ======//
+// console.log(
+// 	   document.getElementById("time").innerHTML
+// 	)
+// ======  DOM GetElemetsByClassName  ======//
+// console.log(
+// 	document.getElementsByClassName("urso")[1]
+// 	)
+// ======  DOM GetElemetsByTagName  ======//
+	// console.log(
+	// 	document.getElementsByTagName("button")[0].innerHTML
+	// 	)
+
+// ======  DOM querySelector  ======//
+
+// console.log(
+// 	document.querySelector("teste2")
+// 	)
+
+// ======  DOM querySelectorAll ======//
+
+	// console.log(
+	// 	document.querySelectorAll(".teste")
+	// 	)
+
+	// console.log(
+		// if (document.getElementById("pai").querySelectorAll(".filho")[0].length > 1)
+		// 	console.log("oi")
+		// )
+
+// ======  DOM createElement ======//
+
+	// console.log(
+	// 	document.createElement("div")
+	// )
+	// var img = document.createElement("img");
+	// img.src="https://picsum.photos/200/300";
+
+	// var p = document.createElement("p");
+	// p.innerHTML = "olá, Mundo"
+
+	// console.log(
+	// 	img,p
+	// 	)
+
+// ======  DOM  appendChild ======//
+
+	// var versa = document.createElement("p");
+	// versa.innerHTML = "versa";
+
+	// var sentra = document.createElement("p");
+	// sentra.innerHTML = "sentra";
+
+	// document.getElementById("nissan").appendChild(versa)
+	// document.getElementById("nissan").appendChild(sentra);
+
+// ======  DOM  removeChild ======//
+
+	// var versa = document.createElement("p");
+	// versa.innerHTML = "versa";
+
+	// var sentra = document.createElement("p");
+	// sentra.innerHTML = "sentra";
+
+	// document.getElementById("nissan").appendChild(versa)
+	// document.getElementById("nissan").appendChild(sentra);
+
+	// document.getElementById("nissan").removeChild(versa);
+
+
+// ======  DOM  parentNode ======//
+
+	// console.log(
+	// 	document.querySelector(".elementaryos").parentNode.parentNode
+	// 	)
+
+// ======  DOM  variaveis ======//
+	
+	// var nome = "paulo";
+	// nome = "outro paulo";
+
+	// console.log(nome);
+
+	// var nome = "pualo";
+	// function teste(){
+	// 	nome = "rafael"
+	// }
+
+	// teste();
+
+	//  console.log(nome);
+
+	// function teste(){
+	// 	let nome = "paulo"
+	// }
+	// teste();
+
+	// console.log(nome);
+	// for( let i = 0);
+
+    const nome = "paulo";
+
+    nome = "rafael";
+
+    console.log(nome);
